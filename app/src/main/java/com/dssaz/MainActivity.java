126fae03de88a713a9125fbd202c10ec3f0ffadd
apple <1766828799@qq.com>
2021-01-07 00:21:04 +0800

    backup

--- app/src/main/java/com/dssaz/MainActivity.java
+++ app/src/main/java/com/dssaz/MainActivity.java
@@ -1,27 +1,24 @@
 package com.dssaz;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Context;
 import android.content.Intent;
-import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import com.dssaz.base.BaseActivity;
 import com.dssaz.db.Exam;
 import com.dssaz.db.ExamDatabase;
-import com.dssaz.db.PeopeoDatabaseHelper;
-import com.dssaz.db.People;
 import com.dssaz.db.User;
 import com.dssaz.ui.ExamineListView;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.j256.ormlite.dao.RuntimeExceptionDao;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
-import java.util.function.Consumer;
 
 public class MainActivity extends BaseActivity {
     private static final String TAG_NAME="MainActivity.username";
@@ -35,7 +32,9 @@ public class MainActivity extends BaseActivity {
     private ExamineListView elv;
     private TextView tv_user;
     private FloatingActionButton fab;
-    RuntimeExceptionDao<Exam, Integer> dao = ExamDatabase.getInstance().getDao();
+    private RuntimeExceptionDao<Exam, Integer> dao = ExamDatabase.getInstance().getDao();
+    private Button btn_pre_page;
+    private Button btn_next_page;
 
     @Override
     protected int setContentViewId() {
@@ -47,6 +46,8 @@ public class MainActivity extends BaseActivity {
         elv=findViewById(R.id.elv_exams);
         tv_user=findViewById(R.id.tv_user);
         fab=findViewById(R.id.fab);
+        btn_pre_page=findViewById(R.id.btn_pre_page);
+        btn_next_page=findViewById(R.id.btn_next_page);
     }
 
     @Override
@@ -57,10 +58,29 @@ public class MainActivity extends BaseActivity {
         }
     }
 
+    private List<List<Exam>> datasSet=new ArrayList<>();
+    private int datasIndex=0;
     @Override
     protected void setView() {
         List<Exam> exams = dao.queryForAll();
-        elv.setItems(exams);
+        Collections.reverse(exams);
+
+        List<Exam> datas=new ArrayList<>();
+        while (!exams.isEmpty()){
+            Exam exam = exams.remove(0);
+            datas.add(exam);
+            if (datas.size()==10){
+                datasSet.add(datas);
+                datas=new ArrayList<>();
+            }
+        }
+
+        if (datas.size()!=0){
+            datasSet.add(datas);
+        }
+
+
+        setExamList();
 
         if (user!=null){
             tv_user.setText("你好:"+user.getUsername()+" ");
@@ -68,6 +88,22 @@ public class MainActivity extends BaseActivity {
 
         boolean b=user!=null&&user.getType()!=1;
         fab.setVisibility(b?View.VISIBLE: View.INVISIBLE);
+    }
+
+    private void setExamList(){
+        if (datasIndex<datasSet.size()){
+            elv.setItems(datasSet.get(datasIndex));
+        }
+        if (datasIndex<=0){
+            btn_pre_page.setEnabled(false);
+        }else{
+            btn_pre_page.setEnabled(true);
+        }
+        if (datasIndex>=(datasSet.size()-1)){
+            btn_next_page.setEnabled(false);
+        }else{
+            btn_next_page.setEnabled(true);
+        }
     }
 
     @Override
@@ -92,6 +128,20 @@ public class MainActivity extends BaseActivity {
                 finish();
             }
         });
+        btn_next_page.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                datasIndex++;
+                setExamList();
+            }
+        });
+        btn_pre_page.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                datasIndex--;
+                setExamList();
+            }
+        });
     }
 
     private long lastBackClicked;
@@ -99,9 +149,10 @@ public class MainActivity extends BaseActivity {
     public void onBackPressed() {
         if ((System.currentTimeMillis()-lastBackClicked)>1000){
             lastBackClicked=System.currentTimeMillis();
-            toast("再次点击后退将退出应用");
+            toast("再次点击后退将返回登录页面");
             return;
         }
+        startActivity(new Intent(mContext,LoginActivity.class));
         finish();
     }
 }
